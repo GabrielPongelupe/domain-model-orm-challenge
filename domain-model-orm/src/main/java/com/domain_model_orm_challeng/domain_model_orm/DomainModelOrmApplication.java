d7325f6fcefaf9029a6286221a2f7b3ef0ecfd1a
GabrielPongelupe <gabrielpongelupee@gmail.com>
2025-02-24 17:02:58 -0300

    Spring Initializr

---
+++ domain-model-orm/src/main/java/com/domain_model_orm_challeng/domain_model_orm/DomainModelOrmApplication.java
@@ -0,0 +1,13 @@
+package com.domain_model_orm_challeng.domain_model_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainModelOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainModelOrmApplication.class, args);
+	}
+
+}
